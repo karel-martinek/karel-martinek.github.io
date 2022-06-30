bfdaadc61c5f62dc44c492e5895185ddee85bc04
Tomas Martinek <sc18t2m@leeds.ac.uk>
2022-06-30 06:08:01 +0100

    Some bugs fixed

--- scripts.js
+++ scripts.js
@@ -1,14 +1,10 @@
-// Smooth scrolling
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
 // Hide navigation menu after button click
 $('.navbar-nav>a').on('click', function(){
     $('.navbar-collapse').collapse('hide');
 });
+
+// Fix navbar button highlighting
+$(".nav-item").on("click", function(){
+    $(".nav-item").removeClass("active");
+    $(this).addClass("active");
+ });
