c3c91bcf448e92dd01544dbd2920abc6c33a4024
Tomas Martinek <sc18t2m@leeds.ac.uk>
2022-06-30 05:25:13 +0100

    Navbar issues sorted out, kinda hacked

---
+++ scripts.js
@@ -0,0 +1,14 @@
+// Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Hide navigation menu after button click
+$('.navbar-nav>a').on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
