5c3f32486d35575797da9e4f2ed32c1f764d96cd
Tomas Martinek <sc18t2m@leeds.ac.uk>
2022-07-01 02:45:25 +0100

    Domu page almost finished

--- scripts.js
+++ scripts.js
@@ -8,3 +8,21 @@ $(".nav-item").on("click", function(){
     $(".nav-item").removeClass("active");
     $(this).addClass("active");
  });
+
+ // Domu buttons click action (scroll to link)
+ $("#domuButton1").on("click", function(){
+    document.getElementById("tesarstvi").scrollIntoView();
+    console.log("Test");
+ })
+ $("#domuButton2").on("click", function(){
+    document.getElementById("truhlarstvi").scrollIntoView();
+    console.log("Test");
+ })
+ $("#domuButton3").on("click", function(){
+    document.getElementById("vyskovePrace").scrollIntoView();
+    console.log("Test");
+ })
+ $("#domuButton4").on("click", function(){
+    document.getElementById("potapecskePrace").scrollIntoView();
+    console.log("Test");
+ })
